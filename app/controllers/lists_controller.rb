1910c9cecc16c54dd85c5719d6ba0beeb13398c1
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-1.compute.internal>
2022-09-16 15:27:49 +0000

    test

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,7 @@
 class ListsController < ApplicationController
-   def new
+  def new
     @list = List.new
-   end
+  end
 
   # 以下を追加
   def create
@@ -14,40 +14,40 @@ class ListsController < ApplicationController
   end
 
   def index
-    @lists=List.all
+    @lists = List.all
   end
 
   def show
-    @list=List.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   def edit
-    @list=List.find(params[:id])
+    @list = List.find(params[:id])
   end
-  
+
   def update
-    list=List.find(params[:id])
+    list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
   def destroy
-    list=List.find(params[:id])
+    list = List.find(params[:id])
     list.destroy
-    redirect_to'/lists'
+    redirect_to "/lists"
   end
-  
+
   def create
-    @list=List.new(list_params)
+    @list = List.new(list_params)
     if @list.save
       redirect_to list_path(@list.id)
     else
       render :new
     end
   end
-  
+
   private
-  def list_params
-    params.require(:list).permit(:title,:body,:image)
-  end
+    def list_params
+      params.require(:list).permit(:title, :body, :image)
+    end
 end
